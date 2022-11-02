12e8e52ceb623ee80d609084ac71f027fc6e81ad
Gary Tang <p2588097@my365.dmu.ac.uk>
2022-11-02 16:27:42 +0000

    moved the getter into the main function to stop mutex from causing errors

--- ShuiJam/Game/src/ShuiJam.cpp
+++ ShuiJam/Game/src/ShuiJam.cpp
@@ -4,12 +4,12 @@
 #include "bitexception.hpp"
 
 WindowManager* gameWindow = new WindowManager(1280, 720, 0, 0, "ShuiJam");
-AudioDevice* audioDevice = AudioDevice::get();
-ALuint testsfx = SoundEffect::get()->addSFX("../SJAssets/Sounds/hitclap.wav");
-SFXSource SFX;
 
 void main()
 {
+	AudioDevice* audioDevice = AudioDevice::get();
+	ALuint testsfx = SoundEffect::get()->addSFX("../SJAssets/Sounds/hitclap.wav");
+	SFXSource SFX;
 
 	SFX.Play(testsfx);
 
