a705e2d03d3253b58428ca023ab6eab65614be19
Lelloq <garytang2011@hotmail.co.uk>
2022-11-06 11:29:17 +0000

    started to write scene

---
+++ ShuiJam/Game/Scenes/Menu/MenuScene.cpp
@@ -0,0 +1,3 @@
+/*\file MenuScene.cpp*/
+#include "MenuScene.h"
+
---
+++ ShuiJam/Game/Scenes/Menu/MenuScene.h
@@ -0,0 +1,10 @@
+/*\file MenuScene.h*/
+#pragma once
+#include "Renderer.h"
+#include "Audio/AudioDevice.h"
+#include "Audio/SoundEffect.h"
+
+class MenuScene
+{
+	
+};
