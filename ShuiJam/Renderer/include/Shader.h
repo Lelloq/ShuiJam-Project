94584d3453bdd0c51ff63d7e0784a9ca461e0cd7
Lelloq <garytang2011@hotmail.co.uk>
2022-11-06 11:21:15 +0000

    comments

--- ShuiJam/Renderer/include/Shader.h
+++ ShuiJam/Renderer/include/Shader.h
@@ -15,8 +15,8 @@ public:
 	Shader(const char* vertexPath, const char* fragPath); //!< Compile the shaders
 	void use();//!< Use the shader
 
-	//Uniform setters
-	void setFloat(const std::string &name, float val);
-	void setInt(const std::string &name, int val);
-	void setBool(const std::string &name, bool val);
+	//!Uniform setters
+	void setFloat(const std::string &name, float val);//!< Set float uniform
+	void setInt(const std::string &name, int val);//!< Set int uniform
+	void setBool(const std::string &name, bool val);//!< Set bool uniform
 };
