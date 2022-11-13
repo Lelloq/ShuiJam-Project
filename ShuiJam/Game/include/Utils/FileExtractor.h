93a51247e3fe5277105db370aa7268fd79d8c0f6
Lelloq <garytang2011@hotmail.co.uk>
2022-11-13 11:05:04 +0000

    moved 7z dll file to sjassets

--- ShuiJam/Game/include/Utils/FileExtractor.h
+++ ShuiJam/Game/include/Utils/FileExtractor.h
@@ -12,7 +12,7 @@ namespace SJ
 	class FileExtractor
 	{
 	private:
-		Bit7zLibrary m_lib{ L"7z.dll" };
+		Bit7zLibrary m_lib{ L"../SJAssets/7z.dll" };
 		BitExtractor m_extractor{ m_lib,BitFormat::Zip };
 		const wchar_t* m_songsFolder = L"../SJAssets/Songs/";
 		const wchar_t* m_inputFolder = L"../SJAssets/Input/";
