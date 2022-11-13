20a4a164646052acac8465d980f74935d8cb8257
Lelloq <garytang2011@hotmail.co.uk>
2022-11-13 21:04:26 +0000

    added todos in file extractor

--- ShuiJam/Game/src/Utils/FileExtractor.cpp
+++ ShuiJam/Game/src/Utils/FileExtractor.cpp
@@ -5,6 +5,7 @@
 //Filter out the non 7 key .osu files within each file
 //Delete files that are not the main audio file
 
+//Currently unfixable bugs - sometimes the song name is too long which makes the bit7z library unable to read the output path
 namespace SJ
 {
 	FileExtractor* FileExtractor::s_extractor{ nullptr };
