b8dc47ccb7a64a55fc20000d349d9cadb9d1076e
Gary Tang <46045360+Lelloq@users.noreply.github.com>
2023-03-27 13:13:12 +0100

    filtering out non 7k modes in processor

--- ShuiJam/Game/src/Utils/FileProcessor.cpp
+++ ShuiJam/Game/src/Utils/FileProcessor.cpp
@@ -14,16 +14,46 @@ namespace SJ
 
 	void FileProcessor::ProcessFiles()
 	{
+		/*to filter out the non 7k maps, in the .osu file mode is 3 and circlesize is 7*/
 		namespace fs = std::filesystem;
 		for(auto& entry : fs::directory_iterator(m_songsFolder))
 		{
+			int mapCount = 0;
 			if(entry.is_directory())
 			{
 				for(auto& osu : fs::directory_iterator(entry))
 				{
-					std::cout << osu << "\n";
+					if(osu.path().extension() == ".osu") 
+					{
+						mapCount++;
+						//std::cout << osu << "\n";
+						std::fstream file;
+						file.open(osu);
+						std::string mode, keymode;
+						//In the osu file the "circlesize" which is the keymode is after the game mode since osu has 4 modes
+						//After it gets the "circlesize" it breaks the loop
+						for(std::string line ; std::getline(file, line); )
+						{
+							if (line.find("Mode") != std::string::npos)
+							{
+								mode = line;
+							}
+							if(line.find("CircleSize") != std::string::npos)
+							{
+								keymode = line;
+								break;
+							}
+						}
+						file.close();
+						if(mode != "Mode: 3" || keymode != "CircleSize:7")
+						{
+							fs::remove(osu);
+							mapCount--;
+						}
+					}
 				}
 			}
+			if (mapCount == 0) fs::remove_all(entry);
 		}
 	}
 }
