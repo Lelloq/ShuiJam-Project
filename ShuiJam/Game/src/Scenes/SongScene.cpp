2d736d207a7856d2d7c4a230e842f3bc89829c5c
Gary Tang <46045360+Lelloq@users.noreply.github.com>
2023-04-07 18:19:18 +0100

    fixed scrolling issue now to fix the song data text changing

--- ShuiJam/Game/src/Scenes/SongScene.cpp
+++ ShuiJam/Game/src/Scenes/SongScene.cpp
@@ -32,7 +32,7 @@ namespace SJ
 		m_selectWheelIm = std::make_shared<Texture>(SJFOLDER + IMAGES + "selectbar.png", GL_CLAMP_TO_EDGE);
 
 		int yPos = 0;
-		for (int i = 0; i < 12; i++)
+		for (int i = 0; i < 11; i++)
 		{
 			m_buttonPositions.push_back(630+yPos);
 			m_buttons.push_back(std::make_unique<Button>(glm::vec2(829, 630+yPos), glm::vec2(451, 57), 0, *m_selectWheelIm));
@@ -86,6 +86,20 @@ namespace SJ
 			m_pixels = 0;
 		}
 
+		//Checking if a part of the song wheel gone past the upper limit or lower limit
+		//The higher number the higher up on the screen
+		for (int i = 0; i < m_buttonPositions.size(); i++)
+		{
+			if (m_buttonPositions.at(i) + 57 >= m_upperLimit && m_scrollDirection == 1)
+			{
+				m_buttonPositions.at(i) = 3;
+			}
+			else if (m_buttonPositions.at(i) <= m_lowerLimit && m_scrollDirection == -1)
+			{
+				m_buttonPositions.at(i) = 687;
+			}
+		}
+
 		//Scrolling the song wheel up or down by 57 pixels (height of a song wheel part)
 		m_slow += dt;
 		if(m_scrollDirection == 1 && m_slow > 0.0005)
@@ -113,18 +127,7 @@ namespace SJ
 			}
 		}
 
-		//Checking if a part of the song wheel gone past the upper limit or lower limit
-		for(int i = 0; i < m_buttonPositions.size(); i++)
-		{
-			if(m_buttonPositions.at(i) > m_upperLimit)
-			{
-				m_buttonPositions.at(i) = 3;
-			}
-			else if(m_buttonPositions.at(i) < m_lowerLimit)
-			{
-				m_buttonPositions.at(i) = 686;
-			}
-		}
+	
 		#pragma endregion
 	}
 	void SongScene::Render()
@@ -195,6 +198,10 @@ namespace SJ
 				m_source->Play(m_scrollSound);
 			}
 			std::async(std::launch::async, &SongScene::updateSongWheel, this);
+		}
+		for(int i = 0; i < m_buttonPositions.size(); i++)
+		{
+			std::cout << i << ": " << m_buttonPositions.at(i) << "\n";
 		}
 	}
 	void SongScene::getMouseButton(int button, int action, int mods)
@@ -304,11 +311,11 @@ namespace SJ
 		else if (m_top < 0) m_top = 11;
 
 		int ptr = m_top;
-		for(int i = m_top; i < m_top + 12; i++)
+		for(int i = m_top; i < m_top + 11; i++)
 		{
 			dbIndices.push_back(i & m_fileProcessor->getLastID());
 			ptr++;
-			ptr = ptr % 12;
+			ptr = ptr % 11;
 			m_songData.at(ptr) = m_fileProcessor->retrieveSong(dbIndices.at(ptr));
 			if (m_songData.at(ptr).title.size() > 30)
 			{
